907eb3192effd5f594bfd108d9f6391bf176a7b2
rondastar <116147793+rondastar@users.noreply.github.com>
2024-01-23 11:29:29 -0800

    Created tests in Main, edited SelectionSort method

--- Program.cs
+++ Program.cs
@@ -10,7 +10,23 @@ namespace GA_SelectionSort_Ronda
     {
         static void Main(string[] args)
         {
-            
+            // test selection sort with int array
+            int[] intArray = { 91, 72, 23, 14, 55, 96, 27, 88, 19, 10, 71, 42, 33, 64, 5 };
+            PrintArray(intArray);
+            SelectionSort(intArray);
+            PrintArray(intArray);
+
+            // test selection sort with char array
+            char[] charArray = { 'z', 'b', 'f', 'y', 'd', 'c', 'k', 'w', 'r' };
+            PrintArray(charArray);
+            SelectionSort(charArray);
+            PrintArray(charArray);
+
+            // test selection sort with string array
+            string[] stringArray = { "This", "is", "a", "grammatical", "sentence,", "but", "it", "won't", "be." };
+            PrintArray(stringArray);
+            SelectionSort(stringArray);
+            PrintArray(stringArray);
         } // Main
         static void SelectionSort<T>(T[] array)
         {
@@ -24,7 +40,7 @@ namespace GA_SelectionSort_Ronda
                 int minIndex = i;
 
                 // Find the minimum element in the remaining unsorted array; j is the unsorted index
-                for (int j = i + 1; j < arrayLength - 1; j++)
+                for (int j = i + 1; j < arrayLength; j++)
                 {
                     if ( Comparer<T>.Default.Compare(array[j], array[minIndex]) < 0)
                     {
@@ -38,5 +54,16 @@ namespace GA_SelectionSort_Ronda
                 array[minIndex] = temporary;
             } // for
         } // SelectionSort
+
+        static void PrintArray<T>(T[] array)
+        {
+            Console.Write("Array: ");
+            foreach (T item in array)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
     } // class
 } // namespace
